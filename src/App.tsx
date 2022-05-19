5988cc00594ea6b18f8059c5fda839236e844868
Lais Camarini <ls.camarini@gmail.com>
2022-05-19 16:00:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Em Construção...</p>
+        <p>
+          <a
+            className="App-link"
+            href="https://github.com/liscamarini"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/liscamarini           
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
